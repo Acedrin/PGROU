a020056feb610b6ada4e0eba68a4259247d6283f
CidTori <sirdeck.homi@hotmail.com>
2016-03-01 17:46:06 +0100

    simplification du WS du cours

--- testsCedric/testWS/client.php
+++ testsCedric/testWS/client.php
@@ -1,15 +1,6 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title></title>
-	</head>
-	<body>
-		<?php 
-			$client = new SoapClient("http://localhost/github/PGROU/testsCedric/testWS/test.wsdl");
-			$var = "Machine";
-			$res = $client->hello(new SoapParam($var, "name"));
-			print $res."<br/>\n";
-		?>
-	</body>
-</html>
+<?php ini_set("soap.wsdl_cache_ttl",1);
+	$client = new SoapClient("http://localhost/github/PGROU/testsCedric/testWS/test.wsdl");
+	$var = "Machine";
+	$res = $client->hello(new SoapParam($var, "name"));
+	print $res."<br/>\n";
+?>
