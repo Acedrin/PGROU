f8f6633107d1b3b3d1394210a0d951adf8956716
CidTori <sirdeck.homi@hotmail.com>
2016-03-03 22:14:56 +0100

    indentation de l'XML (et nettoyage des commentaires)

--- testsCedric/testWStoken/client.php
+++ testsCedric/testWStoken/client.php
@@ -1,54 +1,55 @@
 <?php ini_set("soap.wsdl_cache_ttl",1);
 	$client = new SoapClient("http://localhost/github/PGROU/testsCedric/testWStoken/test.wsdl",array('trace'   => true));
 	
-	//$auth->Username = new SoapVar("username", XSD_STRING, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext");
-	//$auth->Password = new SoapVar("password", XSD_STRING, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext");
-	//$token->UsernameToken = new SoapVar($auth, SOAP_ENC_OBJECT, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", 'UsernameToken', "http://schemas.xmlsoap.org/ws/2002/07/secext");
-	//$UsernameToken = new SoapVar($token, SOAP_ENC_OBJECT, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", 'UsernameToken', "http://schemas.xmlsoap.org/ws/2002/07/secext"); 
-	//$Security = new SoapVar($UsernameToken, SOAP_ENC_OBJECT, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", 'Security', "http://schemas.xmlsoap.org/ws/2002/07/secext");
-	//$client->__setSoapHeaders(new SoapHeader("http://schemas.xmlsoap.org/ws/2002/07/secext","Security",$Security,true,'http://abce.com'));
-	
 	$token = $client->getToken();
-	
-	echo "<pre>";
-	print "getToken request:\n\n";
-	print_r(htmlentities($client->__getLastRequestHeaders()));
-	print_r(htmlentities($client->__getLastRequest()));
-	print "\ngetToken response:\n\n";
-	print_r(htmlentities($client->__getLastResponseHeaders()));
-	print "\n";
-	print_r(htmlentities($client->__getLastResponse()));
-	echo "\n\n</pre>";
+	echo getLastHTTPRequest($client);
+	echo $token."<br/>\n";
 	
 	$client->__setSoapHeaders(new SoapHeader("http://soapinterop.org/echoheader/","setToken",$token));
 	
 	$var = "Machine";
 	$res = $client->hello(new SoapParam($var, "name"));
-	
-	echo "<pre>";
-	print "hello request:\n\n";
-	print_r(htmlentities($client->__getLastRequestHeaders()));
-	print_r(htmlentities($client->__getLastRequest()));
-	print "\nhello response:\n\n";
-	print_r(htmlentities($client->__getLastResponseHeaders()));
-	print "\n";
-	print_r(htmlentities($client->__getLastResponse()));
-	echo "\n\n</pre>";
+	echo getLastHTTPRequest($client);
 	print $res."<br/>\n";
 	
 	sleep(2);
 	
 	$var = "Machine2";
 	$res = $client->hello(new SoapParam($var, "name"));
-	
-	echo "<pre>";
-	print "hello request:\n\n";
-	print_r(htmlentities($client->__getLastRequestHeaders()));
-	print_r(htmlentities($client->__getLastRequest()));
-	print "\nhello response:\n\n";
-	print_r(htmlentities($client->__getLastResponseHeaders()));
-	print "\n";
-	print_r(htmlentities($client->__getLastResponse()));
-	echo "\n\n</pre>";
+	echo getLastHTTPRequest($client);
 	print $res."<br/>\n";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	function format($xml) {
+		$dom = new DomDocument;
+		$dom->loadXML($xml);
+		$dom->formatOutput = true;
+		return $dom->saveXML();	
+	}
+	
+	function getLastHTTPRequest($client) {
+		$string = "";
+		$string .= "<pre>\n";
+		$string .= "*---------------------------------------------------------------------------------\n";
+		$string .= "* request:\n";
+		$string .= "*---------------------------------------------------------------------------------\n";
+		$string .= htmlentities($client->__getLastRequestHeaders()) . "\n";
+		$string .= htmlentities(format($client->__getLastRequest())) . "\n";
+		$string .= "*---------------------------------------------------------------------------------\n";
+		$string .= "* response:\n";
+		$string .= "*---------------------------------------------------------------------------------\n";
+		$string .= htmlentities($client->__getLastResponseHeaders()) . "\n";
+		$string .= htmlentities(format($client->__getLastResponse())) . "\n";
+		$string .= "*---------------------------------------------------------------------------------\n";
+		$string .= "</pre>";
+		return $string;
+	}
+
 ?>
