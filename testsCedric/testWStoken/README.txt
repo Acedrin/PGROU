1aac0c9203aa55ee5ca19c9a58f38f992e2e1611
CidTori <sirdeck.homi@hotmail.com>
2016-03-03 16:18:39 +0100

    affichage

---
+++ testsCedric/testWStoken/README.txt
@@ -0,0 +1,1 @@
+http://localhost/github/PGROU/testsCedric/testWStoken/client.php
--- testsCedric/testWStoken/client.php
+++ testsCedric/testWStoken/client.php
@@ -1,6 +1,15 @@
 <?php ini_set("soap.wsdl_cache_ttl",1);
 	$client = new SoapClient("http://localhost/github/PGROU/testsCedric/testWStoken/test.wsdl",array('trace'   => true));
+	
+	//$auth->Username = new SoapVar("username", XSD_STRING, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext");
+	//$auth->Password = new SoapVar("password", XSD_STRING, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext");
+	//$token->UsernameToken = new SoapVar($auth, SOAP_ENC_OBJECT, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", 'UsernameToken', "http://schemas.xmlsoap.org/ws/2002/07/secext");
+	//$UsernameToken = new SoapVar($token, SOAP_ENC_OBJECT, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", 'UsernameToken', "http://schemas.xmlsoap.org/ws/2002/07/secext"); 
+	//$Security = new SoapVar($UsernameToken, SOAP_ENC_OBJECT, NULL, "http://schemas.xmlsoap.org/ws/2002/07/secext", 'Security', "http://schemas.xmlsoap.org/ws/2002/07/secext");
+	//$client->__setSoapHeaders(new SoapHeader("http://schemas.xmlsoap.org/ws/2002/07/secext","Security",$Security,true,'http://abce.com'));
+	
 	$token = $client->getToken();
+	
 	echo "<pre>";
 	print "getToken request:\n\n";
 	print_r(htmlentities($client->__getLastRequestHeaders()));
@@ -10,10 +19,12 @@
 	print "\n";
 	print_r(htmlentities($client->__getLastResponse()));
 	echo "\n\n</pre>";
+	
 	$client->__setSoapHeaders(new SoapHeader("http://soapinterop.org/echoheader/","setToken",$token));
 	
 	$var = "Machine";
 	$res = $client->hello(new SoapParam($var, "name"));
+	
 	echo "<pre>";
 	print "hello request:\n\n";
 	print_r(htmlentities($client->__getLastRequestHeaders()));
@@ -29,6 +40,7 @@
 	
 	$var = "Machine2";
 	$res = $client->hello(new SoapParam($var, "name"));
+	
 	echo "<pre>";
 	print "hello request:\n\n";
 	print_r(htmlentities($client->__getLastRequestHeaders()));
