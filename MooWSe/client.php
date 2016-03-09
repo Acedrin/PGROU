2001bf1014db78972a43315eef3bb5059d297a0c
quent <quent@DESKTOP-CEPC5LU>
2016-03-09 09:36:06 +0100

    commit ajout des hashs dans le client

--- MooWSe/client.php
+++ MooWSe/client.php
@@ -1,150 +1,160 @@
-<?php ini_set("soap.wsdl_cache_ttl",1);
+<?php
 
-	//echo bin2hex(openssl_random_pseudo_bytes(8,true));
-        //salut c'est Quentin !
-	$username = "client".","."modalite";
-	$password = "password";
-	
-	$options = array(
-		//"location" => NULL,
-		//"uri" => NULL,
-		//"style" => NULL,
-		//"use" => NULL,
-		//"soap_version" => SOAP_1_1,
-		//"login" => $login,
-		//"password" => $password,
-		//"proxy_host" => NULL,
-		//"proxy_port" => NULL,
-		//"proxy_login" => NULL,
-		//"proxy_password" => NULL,
-		//"local_cert" => NULL,
-		//"passphrase" => NULL,
-		//"authentication" =>  SOAP_AUTHENTICATION_DIGEST,
-		//"compression" => NULL,
-		//"encoding" => NULL,
-        "trace" => true,
-		//"classmap" => array("wsdl_type" => "php_class"),
-        "exceptions"=>true,
+ini_set("soap.wsdl_cache_ttl", 1);
+
+//echo bin2hex(openssl_random_pseudo_bytes(8,true));
+//Ici le client entre ses mots de passe en brut (ce fichier n'est pas accessible par un pirate, seules les informations transmises le sont)
+
+$username = "client" . "," . "modalite";
+$password = "password";
+
+//Il faut maintenant hacher une première fois le mot de passe (on utilisera simplement sha1 pour le moment)
+
+$hashedPassword=sha1($password);
+
+//génération d'un sel (aléatoire pour le moment, il est possible d'ajouter la date), le sel va permettre de brouiller le mot de passe aux yeux d'un HDM
+
+$salt=password_hash();
+
+//cryatge du mot de passe avec la fonction crypt (incassable, mais récupérable)
+
+$encryptedPassword=crypt($hashedPassword,$salt); //mot de passe crypté
+
+$options = array(
+    //"location" => NULL,
+    //"uri" => NULL,
+    //"style" => NULL,
+    //"use" => NULL,
+    //"soap_version" => SOAP_1_1,
+    //"login" => $login,
+    //"password" => $password,
+    //"proxy_host" => NULL,
+    //"proxy_port" => NULL,
+    //"proxy_login" => NULL,
+    //"proxy_password" => NULL,
+    //"local_cert" => NULL,
+    //"passphrase" => NULL,
+    //"authentication" =>  SOAP_AUTHENTICATION_DIGEST,
+    //"compression" => NULL,
+    //"encoding" => NULL,
+    "trace" => true,
+    //"classmap" => array("wsdl_type" => "php_class"),
+    "exceptions" => true,
         //"connection_timeout"=>5,
-		//"typemap" => array(
-		//	"type_name" => NULL,
-		//	"type_ns" => NULL,
-		//	"from_xml" => NULL,
-		//	"to_xml" => NULL
-		//),
-		//"cache_wsdl" => NULL,
-		//"user_agent" => NULL,
-		//"stream_context" => NULL,
-		//"features" => NULL,
-		//"keep_alive" => NULL,
-		//"ssl_method" => NULL,
-    );
-	
-	try {
-		$MooWSe_WSDL = "http://localhost/github/PGROU/MooWSe/MooWSe.wsdl";
-		$MooWSe_client = new SoapClient($MooWSe_WSDL,$options);
-	} catch (SoapFault $fault) {
-		trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
-	}
-	
-	$wsse = "http://schemas.xmlsoap.org/ws/2002/07/secext";
-	$mustUnderstand = true; // oblige le rÃ©cepteur Ã  traiter l'header
-	$actor = "http://schemas.xmlsoap.org/soap/actor/next"; // le destinataire est le premier rÃ©cepteur
-	//$actor = "http://localhost/github/PGROU/testsCedric/testWStoken/server.php"; // le destinataire est ce rÃ©cepteur (ne marche pas)
-	//$actor = ""; // le destinataire est le dernier rÃ©cepteur (marche mais lance un warning)
-	
-	$Username = $username;
-	$Password = $password;
-	$UsernameToken["Username"] = new SoapVar($Username, XSD_STRING, NULL, $wsse, NULL, $wsse);
-	$UsernameToken["Password"] = new SoapVar($Password, XSD_STRING, "PasswordText", $wsse, NULL, $wsse);
-	$Security = array();
-	$Security["UsernameToken"] = new SoapVar($UsernameToken, SOAP_ENC_OBJECT, NULL, $wsse, NULL, $wsse);
-	$Security = new SoapVar($Security, SOAP_ENC_OBJECT, NULL, $wsse, NULL, $wsse);
-	$Header = array();
-	$Header[] = new SoapHeader($wsse,"Security",$Security,$mustUnderstand,$actor);
-	$MooWSe_client->__setSoapHeaders(NULL);
-	$MooWSe_client->__setSoapHeaders($Header);
-	
-	//print_r_pre($Header);
-	
-	try {
-		$token = $MooWSe_client->authenticate();
-	} catch (SoapFault $fault) {
-		trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
-	}
-	
-	echo getLastHTTPDialogue($MooWSe_client);
-	echo "<pre>".htmlspecialchars($token)."</pre>";
-	
-	$Security = array();
-	$Security["BinarySecurityToken"] = new SoapVar($token, XSD_STRING, NULL, $wsse, NULL, $wsse);
-	$Security = new SoapVar($Security, SOAP_ENC_OBJECT, NULL, $wsse, NULL, $wsse);
-	$Header = array();
-	$Header[] = new SoapHeader($wsse,"Security",$Security,$mustUnderstand,$actor);
-	$MooWSe_client->__setSoapHeaders(NULL);
-	$MooWSe_client->__setSoapHeaders($Header);
-	
-	try {
-		$service = "service";
-		$service_WSDL = htmlspecialchars_decode($MooWSe_client->getWSDL(new SoapParam($service, "service")),ENT_XML1);
-	} catch (SoapFault $fault) {
-		trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
-	}
-	
-	echo getLastHTTPDialogue($MooWSe_client);
-	echo "<pre>".htmlspecialchars($service_WSDL)."</pre>";
-	
-	sleep(2);
-	
-	try {
-		$service = "service2";
-		$service_WSDL = htmlspecialchars_decode($MooWSe_client->getWSDL(new SoapParam($service, "service")),ENT_XML1);
-	} catch (SoapFault $fault) {
-		trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
-	}
-	
-	echo getLastHTTPDialogue($MooWSe_client);
-	echo "<pre>".htmlspecialchars($service_WSDL)."</pre>";
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	function format($xml) {
-		$dom = new DomDocument;
-		$dom->loadXML($xml);
-		$dom->formatOutput = true;
-		return $dom->saveXML();	
-	}
-	
-	function getLastHTTPDialogue($client) {
-		$string = "";
-		$string .= "<pre>\n";
-		$string .= "*---------------------------------------------------------------------------------\n";
-		$string .= "* request:\n";
-		$string .= "*---------------------------------------------------------------------------------\n";
-		$string .= htmlentities($client->__getLastRequestHeaders()) . "\n";
-		$string .= htmlentities(format($client->__getLastRequest())) . "\n";
-		$string .= "*---------------------------------------------------------------------------------\n";
-		$string .= "* response:\n";
-		$string .= "*---------------------------------------------------------------------------------\n";
-		$string .= htmlentities($client->__getLastResponseHeaders()) . "\n";
-		$string .= htmlentities(format($client->__getLastResponse())) . "\n";
-		$string .= "*---------------------------------------------------------------------------------\n";
-		$string .= "</pre>";
-		return $string;
-	}
-	
-	function print_r_pre($thing) {
-		echo "<pre>";
-		print_r($thing);
-		echo "</pre>";
-	}
+        //"typemap" => array(
+        //	"type_name" => NULL,
+        //	"type_ns" => NULL,
+        //	"from_xml" => NULL,
+        //	"to_xml" => NULL
+        //),
+        //"cache_wsdl" => NULL,
+        //"user_agent" => NULL,
+        //"stream_context" => NULL,
+        //"features" => NULL,
+        //"keep_alive" => NULL,
+        //"ssl_method" => NULL,
+);
+
+try {
+    $MooWSe_WSDL = "http://localhost/github/PGROU/MooWSe/MooWSe.wsdl";
+    $MooWSe_client = new SoapClient($MooWSe_WSDL, $options);
+} catch (SoapFault $fault) {
+    trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
+}
+
+$wsse = "http://schemas.xmlsoap.org/ws/2002/07/secext";
+$mustUnderstand = true; // oblige le rÃ©cepteur Ã  traiter l'header
+$actor = "http://schemas.xmlsoap.org/soap/actor/next"; // le destinataire est le premier rÃ©cepteur
+//$actor = "http://localhost/github/PGROU/testsCedric/testWStoken/server.php"; // le destinataire est ce rÃ©cepteur (ne marche pas)
+//$actor = ""; // le destinataire est le dernier rÃ©cepteur (marche mais lance un warning)
+
+$Username = $username;
+$Password = $encryptedPassword; //on envoie le mot de passe crypté
+$Salt=$salt; //ajout du sel
+
+$UsernameToken["Username"] = new SoapVar($Username, XSD_STRING, NULL, $wsse, NULL, $wsse);
+$UsernameToken["Password"] = new SoapVar($Password, XSD_STRING, "PasswordText", $wsse, NULL, $wsse);
+//il faut aussi envoyer le sel
+$UsernameToken["Salt"] = new SoapVar($Salt, XSD_STRING, "SaltText", $wsse, NULL, $wsse);
+
+$Security = array();
+$Security["UsernameToken"] = new SoapVar($UsernameToken, SOAP_ENC_OBJECT, NULL, $wsse, NULL, $wsse);
+$Security = new SoapVar($Security, SOAP_ENC_OBJECT, NULL, $wsse, NULL, $wsse);
+$Header = array();
+$Header[] = new SoapHeader($wsse, "Security", $Security, $mustUnderstand, $actor);
+$MooWSe_client->__setSoapHeaders(NULL);
+$MooWSe_client->__setSoapHeaders($Header);
+
+//print_r_pre($Header);
+
+try {
+    $token = $MooWSe_client->authenticate();
+} catch (SoapFault $fault) {
+    trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
+}
+
+echo getLastHTTPDialogue($MooWSe_client);
+echo "<pre>" . htmlspecialchars($token) . "</pre>";
+
+$Security = array();
+$Security["BinarySecurityToken"] = new SoapVar($token, XSD_STRING, NULL, $wsse, NULL, $wsse);
+$Security = new SoapVar($Security, SOAP_ENC_OBJECT, NULL, $wsse, NULL, $wsse);
+$Header = array();
+$Header[] = new SoapHeader($wsse, "Security", $Security, $mustUnderstand, $actor);
+$MooWSe_client->__setSoapHeaders(NULL);
+$MooWSe_client->__setSoapHeaders($Header);
+
+try {
+    $service = "service";
+    $service_WSDL = htmlspecialchars_decode($MooWSe_client->getWSDL(new SoapParam($service, "service")), ENT_XML1);
+} catch (SoapFault $fault) {
+    trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
+}
+
+echo getLastHTTPDialogue($MooWSe_client);
+echo "<pre>" . htmlspecialchars($service_WSDL) . "</pre>";
+
+sleep(2);
+
+try {
+    $service = "service2";
+    $service_WSDL = htmlspecialchars_decode($MooWSe_client->getWSDL(new SoapParam($service, "service")), ENT_XML1);
+} catch (SoapFault $fault) {
+    trigger_error("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})", E_USER_ERROR);
+}
+
+echo getLastHTTPDialogue($MooWSe_client);
+echo "<pre>" . htmlspecialchars($service_WSDL) . "</pre>";
+
+function format($xml) {
+    $dom = new DomDocument;
+    $dom->loadXML($xml);
+    $dom->formatOutput = true;
+    return $dom->saveXML();
+}
+
+function getLastHTTPDialogue($client) {
+    $string = "";
+    $string .= "<pre>\n";
+    $string .= "*---------------------------------------------------------------------------------\n";
+    $string .= "* request:\n";
+    $string .= "*---------------------------------------------------------------------------------\n";
+    $string .= htmlentities($client->__getLastRequestHeaders()) . "\n";
+    $string .= htmlentities(format($client->__getLastRequest())) . "\n";
+    $string .= "*---------------------------------------------------------------------------------\n";
+    $string .= "* response:\n";
+    $string .= "*---------------------------------------------------------------------------------\n";
+    $string .= htmlentities($client->__getLastResponseHeaders()) . "\n";
+    $string .= htmlentities(format($client->__getLastResponse())) . "\n";
+    $string .= "*---------------------------------------------------------------------------------\n";
+    $string .= "</pre>";
+    return $string;
+}
+
+function print_r_pre($thing) {
+    echo "<pre>";
+    print_r($thing);
+    echo "</pre>";
+}
 
 ?>
