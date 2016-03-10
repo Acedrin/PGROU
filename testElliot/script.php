f65dcca0dc5d2be7f13ea8a2ef50ecfd4ed20721
unknown <elliot41@bitbucket.org>
2016-03-10 22:05:33 +0100

    creation du generateur de wsdl

---
+++ testElliot/script.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Générateur WSDL
+ * Author : Elliot Catherin
+ */
+
+include('generateWSDL.php');
+
+
+
+
+$array=array(
+    1=>1,
+    2=>2
+    //3=>3
+    );
+
+generateWSDL($array);
+
+
