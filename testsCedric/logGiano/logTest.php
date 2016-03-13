1b373bfbfd72fe44c553fff7fd311bbde671c973
gianoberto giampieri <giano.giampieri@gmail.com>
2016-03-13 15:33:13 +0100

    maded some tests

--- testsCedric/logGiano/logTest.php
+++ testsCedric/logGiano/logTest.php
@@ -6,8 +6,8 @@ include('logRotate.php');
 $bar = new Logger();
 
 
-/*$bar->LogError("NOME","127.0.0.1","token","error");
-$bar->LogFunc("NOME","127.0.0.1","token","function");*/
+$bar->LogError("NOME","127.0.0.1","token","error");
+$bar->LogFunc("NOME","127.0.0.1","token","function");
 $bar->LogUser("NOME","127.0.0.1","token");
 
 //$bar->maxAgedFile('./logs/userLog');
