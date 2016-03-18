5926df961eebf14ac21fd081b39eb1ece21f3f0b
quent <quent@DESKTOP-CEPC5LU>
2016-03-18 12:04:35 +0100

    listFunction selective

--- MooWSe/Moteur/dataBaseCall.php
+++ MooWSe/Moteur/dataBaseCall.php
@@ -50,31 +50,35 @@ class dataBaseCall {
     }
 
     //prend en argument le nom du client et renvoit un array contenant les ids des fonctions auxquelles le client peut accèder
-    function listFunction($client_name, $service) {
+    //prend en argument le nom du client et renvoit un array contenant les ids des fonctions auxquelles le client peut accÃ¨der
+    function listFunction($client_name,$service) {
         //appel des fonctions autorisees pour le client
         $base = $this->bdd;
-        $function_request = $base->query('SELECT function.function_id FROM function INNER JOIN access ON function.function_id=access.function_id
+         $function_request = $base->query('SELECT function.function_id FROM function INNER JOIN access ON function.function_id=access.function_id
                     INNER JOIN client ON access.client_id=client.client_id
                     INNER JOIN server ON server.server_id=function.server_id
                     WHERE client_name=\'' . $client_name . '\' AND server_name=\'' . $service . '\'');
-        //extraire le premier element              
-        $current_function = $function_request->fetch();
-        //creation d'un array
-        $functions = array($current_function["function_id"]);
-        //On garde l'id
-        $last_function = $current_function["function_id"];
-        //parcours de la table des fonctions autorisees
-        while ($current_function = $function_request->fetch()) {
-            //ajoute la fonction courante au array
-            if ($current_function["function_id"] != $last_function) {
-                array_push($functions, $current_function["function_id"]);
-            }
+        //extraire le premier element, s'il y en a un on extrait les autres et on les met dans un array              
+        if ($current_function = $function_request->fetch()) {
+            //creation d'un array
+            $functions = array($current_function["function_id"]);
+            //On garde l'id
             $last_function = $current_function["function_id"];
+            //parcours de la table des fonctions autorisees
+            while ($current_function = $function_request->fetch()) {
+                //ajoute la fonction courante au array
+                if ($current_function["function_id"] != $last_function) {
+                    array_push($functions, $current_function["function_id"]);
+                }
+                $last_function = $current_function["function_id"];
+            }
         }
-        //on renvoit l'array des fonctions autorisées
+        else{
+            //on renvoie un array vide
+            $functions=array();
+        }
         return $functions;
     }
-
     function affichage($client_name) {
         //appel de la base
         $base = $this->bdd;
@@ -83,11 +87,8 @@ class dataBaseCall {
                     INNER JOIN client ON access.client_id=client.client_id
                     INNER JOIN server ON server.server_id=function.server_id
                     WHERE client_name=\'' . $client_name . '\' ');
-        //obtention du nombre de fonctions auxquelles le cl
-        $number_function = $function_request->rowCount();
-        echo '<h2>Affichage des fonctions auxquelles ' . $client_name . ' vous peut acceder</h2>';
-        echo '</br>';
-        echo 'Le client ' . $client_name . ' peut acceder a ' . $number_function . ' fonction(s)';
+
+        echo '<h2>Affichage des fonctions auxquelles le client ' . $client_name . ' peut acceder</h2>';
         echo '</br>';
         echo '<u><b>Listing des fonctions :</u></b>';
         echo '</br>';
@@ -96,16 +97,19 @@ class dataBaseCall {
             if ($function_recup != $last_function) {
                 echo 'fonction numero: ' . $function_recup["function_id"] . ' ' . $function_recup["function_name"] . ' du serveur ' . $function_recup["server_name"];
                 echo '</br>';
-                echo 'qui prend en argument : ';
-                echo '</br>';
                 //aller chercher les variables de la fonction
-                $variable_request = $base->query('SELECT variable.variable_name,type.type_name FROM variable INNER JOIN type ON type.type_id=variable.variable_id
+                $variable_request = $base->query('SELECT variable.variable_name,type.type_name,variable.variable_input FROM variable INNER JOIN type ON type.type_id=variable.type_id
                     WHERE variable.function_id=\'' . $function_recup["function_id"] . '\' ');
                 while ($variable_recup = $variable_request->fetch()) {
                     $variable_name = $variable_recup["variable_name"];
                     $type_name = $variable_recup["type_name"];
-                    echo 'variable ' . $variable_name . ' de type ' . $type_name;
-                    echo '</br>';
+                    $variable_input = $variable_recup["variable_input"];
+                    if ($variable_input == 1) {
+                        echo'<div style="text-indent: 15px;">Argument(s) :</div>';
+                    } else {
+                        echo'<div style="text-indent: 15px;">Sortie :</div>';
+                    }
+                    echo '<div style="text-indent: 30px;">variable ' . $variable_name . ' de type ' . $type_name . '</div>';
                 }
                 echo '</br>';
                 //fin de recherche des variables
